2584bb1472980acd90a775a0bf10f48a360e56b4
erickyun <71968711+erickyun@users.noreply.github.com>
2025-04-28 14:37:53 +0300

    Update gofile.go

--- hosts/gofile/gofile.go
+++ hosts/gofile/gofile.go
@@ -11,24 +11,43 @@ const (
 	serverUrl = "https://api.gofile.io/servers"
 )
 
+type GetServer struct {
+	Status string `json:"status"`
+	Data   struct {
+		Servers map[string]struct {
+			Name string `json:"name"`
+		} `json:"servers"`
+	} `json:"data"`
+}
+
+type Upload struct {
+	Status string `json:"status"`
+	Data   struct {
+		DownloadPage string `json:"downloadPage"`
+	} `json:"data"`
+}
+
 func getServer() (string, error) {
-    	respBody, err := utils.DoGet(serverUrl, nil, nil)
-    	if err != nil {
-        	return "", err
-    	}
-    	defer respBody.Close()
-    	var obj GetServer
-    	err = json.NewDecoder(respBody).Decode(&obj)
-    	if err != nil {
-        	return "", err
-    	}
-    	if obj.Status != "ok" {
-        	return "", errors.New("Bad response.")
-    	}
-    	if len(obj.Data.Servers) == 0 {
-        	return "", errors.New("No servers available.")
-    	}
-    	return obj.Data.Servers[0].Name, nil
+	respBody, err := utils.DoGet(serverUrl, nil, nil)
+	if err != nil {
+		return "", err
+	}
+	defer respBody.Close()
+
+	var obj GetServer
+	if err := json.NewDecoder(respBody).Decode(&obj); err != nil {
+		return "", err
+	}
+	if obj.Status != "ok" {
+		return "", errors.New("bad response from server")
+	}
+	if len(obj.Data.Servers) == 0 {
+		return "", errors.New("no servers available")
+	}
+	for _, server := range obj.Data.Servers {
+		return server.Name, nil
+	}
+	return "", errors.New("no servers found")
 }
 
 func upload(uploadUrl, path string, size, byteLimit int64, headers map[string]string) (string, error) {
@@ -37,13 +56,13 @@ func upload(uploadUrl, path string, size, byteLimit int64, headers map[string]st
 		return "", err
 	}
 	defer respBody.Close()
+
 	var obj Upload
-	err = json.NewDecoder(respBody).Decode(&obj)
-	if err != nil {
+	if err := json.NewDecoder(respBody).Decode(&obj); err != nil {
 		return "", err
 	}
 	if obj.Status != "ok" {
-		return "", errors.New("Bad response.")
+		return "", errors.New("bad response during upload")
 	}
 	return obj.Data.DownloadPage, nil
 }
@@ -53,14 +72,22 @@ func Run(args *utils.Args, path string) (string, error) {
 	if err != nil {
 		return "", err
 	}
+
 	server, err := getServer()
 	if err != nil {
 		return "", err
 	}
+
 	uploadUrl := "https://" + server + ".gofile.io/uploadFile"
+
 	headers := map[string]string{
 		"Referer": referer,
 	}
+
 	url, err := upload(uploadUrl, path, size, args.ByteLimit, headers)
-	return url, err
+	if err != nil {
+		return "", err
+	}
+
+	return url, nil
 }
